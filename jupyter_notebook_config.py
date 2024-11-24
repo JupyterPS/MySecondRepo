7a6fe37e62b246a063ad739922826ee7655a641b
Henning Reckey <69686320+JupyterPS@users.noreply.github.com>
2024-11-24 10:31:20 +0100

    Delete jupyter_notebook_config.py

--- jupyter_notebook_config.py
+++
@@ -1,8 +0,0 @@
-c = get_config()  # Initialize configuration
-c.NotebookApp.ip = '0.0.0.0'
-c.NotebookApp.port = 8888
-c.NotebookApp.open_browser = False
-c.NotebookApp.allow_root = True
-c.NotebookApp.token = ''
-c.NotebookApp.log_level = 'DEBUG'
-c.NotebookApp.log_file = '/home/jovyan/logs/jupyter.log'
